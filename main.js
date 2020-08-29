695ac94ee5c0df9e85ac9eaa0c746c71c397ec5e
vancovermobi <70420449+vancovermobi@users.noreply.github.com>
2020-08-29 15:43:22 +0700

    update Xirsys

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
+//import Peer from 'peerjs';
 //const socket = io('http://localhost:3000');
 const socket = io('https://stream3005phamdinh01.herokuapp.com/');
-$('#div-chat').hide();
+
+
+$('#div-chat').hide(); // Khi khởi động lên Ẩn div_Chat
+
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "vancovermobi",
+    secret: "f2915c82-e9d0-11ea-b83d-0242ac150002",
+    domain: "vancovermobi.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
 
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
-    $('#div-chat').show();
+    $('#div-chat').show();   // đăng nhập thành công => hiện div_chat
     $('#div-dang-ky').hide();
 
-    console.log(arrUserInfo);
-    arrUserInfo.forEach(user => {
+    // console.log(arrUserInfo); // hiện danh sách tên username kiểu Mảng
+    arrUserInfo.forEach(user => {   
         const { ten, peerId } = user;
         $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);    
     });
@@ -36,22 +59,19 @@ function playStream(idVideoTag, stream){
 // openStream()
 // .then(stream => playStream('localStream', stream));
 
-//const peer = new Peer ();
-//const peer = new Peer({key: '26pce8r38qi00000' }); 
-//peer.on('open', id => console.log(id));
+// const peer = new Peer ();  // lấy id từ peerjs cấp ngẫu nhiên ,mõi lần chạy
 const peer = new Peer({ 
     host : "0.peerjs.com" , 
     secure : false , 
-    initiator: true
-    
+    initiator: true,
+    config: customConfig 
 });
-
 peer.on('open', id => {
-    $('#my-peer').append(id)
+    $('#my-peer').append(id)  
     // btn SignUp
     $('#btnSignUp').click(() => {
     const username = $('#txtUsername').val();
-    socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerId: id});
+    socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerId: id}); // gán mõi username là 1 peerId
     });
 });
 
